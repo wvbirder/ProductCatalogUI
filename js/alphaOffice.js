80e7f54498ea0420b47a05fc9ec86f0f483a8b23
jhennen <jhennen>
2017-09-24 18:44:05 -0500

    September 24 modifications

--- js/alphaOffice.js
+++ js/alphaOffice.js
@@ -1,33 +1,34 @@
-/* .js for the Node.js Applicaiton.*/
+/* Javascript .js file for the alpha.html page */
 
 /*********************************************************    
 CHANGE THE URLS BELOW FOR THE WORKSHOP - THESE URLS MUST POINT TO THE MICROSERVICES.      
 *********************************************************/ 
-var dbServiceURL = "https://alphaofficemysqlrest-gse00010691.apaas.em3.oraclecloud.com/products";
-//"http://localhost:8002"; // THIS URL IS FOR THE CLIENT TEST ONLY. REPLACE THIS WITH THE MYSQL APPLICATION CLOUD CONTAINER URL TO RUN IN THE CLOUD.
-var tweetServiceBaseURL = "https://javatwittermicroservice-gse00010691.apaas.em3.oraclecloud.com/statictweets/%23";
-//"http://localhost:8003"; // THIS URL IS FOR THE CLIENT TEST ONLY. REPLACE THIS WITH THE TWITTER APPLICATION CLOUD CONTAINER URL TO RUN IN THE CLOUD.
+// URL for the MySQL database REST service
+var dbServiceURL = "https://notactivated";
+
+// URL for the Twitter REST service
+var tweetServiceBaseURL = "https://notactivated";
 /*********************************************************    
 CHANGE THE URLS ABOVE FOR THE WORKSHOP.    
 *********************************************************/ 
 
-// Set position and size variables
+// Set position and size variables.
 var popupTopVar = 20;
 var popupTwitterContentHeightVar = 490;
 
-// Set constants used in annimation
+// Set constants used in animation.
 var transitionCurveVar = 1.05;
 var transitionPositionStepsVar = 30;
 var transitionSizeStepsVar = 10;;
 var widthIncrementVar = 30;
 var heightIncrementVar = 30;
 
-// Some browsers (e.g. Chrome) process annimations faster than others - set some browsers slower and some faster
+// Some browsers (e.g. Chrome) process animations faster than others - set some browsers slower and some faster.
 var browserTransitionIntervalArray = new Array();
 browserTransitionIntervalArray["Firefox"] = 7;
-browserTransitionIntervalArray["IE"] = 3;
+browserTransitionIntervalArray["IE"] = 2;
 browserTransitionIntervalArray["Chrome"] = 11;
-browserTransitionIntervalArray["Safari"] = 5;
+browserTransitionIntervalArray["Safari"] = 7;
 var transitionIntervalVar = browserTransitionIntervalArray[getBrowser()];
 
 // Holds json for future calls to manipulate data
@@ -43,8 +44,12 @@ var hashtagVar;
 var tweetTableTemplateVar;
 
 $(document).ready(function () {
-    // Call REST service Node.js accessing MySQL product data
-//    $.getJSON('js/sample.json', function (data) {
+    // If the dbServiceURL has not been changed ...
+    if (dbServiceURL == "https://notactivated") {
+        document.getElementById("productDiv").innerHTML = document.getElementById("noMySQLRESTURLFillerDiv").innerHTML;
+        return;
+    }
+    // Call REST service Node.js accessing MySQL product data.
     $.getJSON(dbServiceURL, function (data) {
         try{
             holder =data;
@@ -55,9 +60,12 @@ $(document).ready(function () {
         }
     });
 
-    // Build HTML from the JSON Feed.*/
-    function buildHTML() {
-        tweetTableTemplateVar = document.getElementById("tweetTableFillerDiv").innerHTML;
+// Build HTML from the JSON Feed.
+    // Create a template for the product panels.  There are 4 product panels per row on the main page.  In order have the popup panel used in the animation look identical to the 
+    // product panels, the product panel template is extracted from the popup panel.  The template is contained in popupHTLMArray[0] and popupHTLMArray[1].
+    // The generic template is created by removing syntax specific to the popup panel.
+    function buildHTML() {    
+        tweetTableTemplateVar = document.getElementById("tweetTableFillerDiv").innerHTML; // To be used later for formatting Tweet data in the popup
         popupHTLMArray = document.getElementById("popupTable").innerHTML.split("~");
         popupHTLMArray[0] = popupHTLMArray[0].replace(" id=\"popupProductC4R1Spacer\"", "");
         popupHTLMArray[0] = popupHTLMArray[0].replace(" id=\"popupProductContentDiv\"", "");
@@ -66,7 +74,7 @@ $(document).ready(function () {
         popupHTLMArray[1] = popupHTLMArray[1].replace(" id=\"popupControlTd\"", "");
         indexVar = 0;
         try {
-    //Loop through JSON 
+        //Loop through JSON and populate productArray.
         $.each(holder.Products, function(index, details) { 
             productArray[details.PRODUCT_ID] = {parent_category_id: details.PARENT_CATEGORY_ID, category_id: details.CATEGORY_ID,
                 product_name: details.PRODUCT_NAME, product_status: details.PRODUCT_STATUS, list_price: details.LIST_PRICE,
@@ -74,6 +82,7 @@ $(document).ready(function () {
             productIndexArray[indexVar] = details.PRODUCT_ID;             
             indexVar = indexVar + 1;                   
         });
+        // Create table HTML from productArray.
         var columnNumberConst = 4;
         var tableVar = "<table id=\"allTable\" style=\"border-spacing:0px\">";
         for (i = 0; i < productIndexArray.length; i++) {
@@ -92,12 +101,12 @@ $(document).ready(function () {
             } else if (i % columnNumberConst == (columnNumberConst - 1)) {
                 tableVar = tableVar + "</tr>";
             }
-        }
-        tableVar = tableVar + "</table>";  
-        var html_holder= "<div id=\"myProducts\">" + tableVar + "</div>";            
-        console.log(html_holder);
-        //display to page the new HTML
-        $('#myProducts').replaceWith(html_holder);
+        }        
+        tableVar = tableVar.replace(/\(R\)/g, "&reg;");       
+        tableVar = tableVar.replace(/[^\x00-\x7F]/g, "");                      
+        tableVar = tableVar + "</table>";           
+        // Write table HTML to productDiv.
+        document.getElementById("productDiv").innerHTML = tableVar;
         }
         catch(err){
             console.log("Error parsing the data in the JSON file");
@@ -118,8 +127,15 @@ function diplayPrice(priceParm) {
     return stringVar;
 }
 
+// Start animation on click of a product panel.
 function selectProduct(idParm) {  
+    // Prevent selecting a second product while first product is still open.
     if (lockVar) {
+        return;
+    }  
+    // If Twitter REST service URL has not been updated 
+    if (tweetServiceBaseURL == "https://notactivated") {
+        alert("You have not yet replaced the URL for the Twitter REST service with the appropriate one for your deployment environment.");
         return;
     }
     lockVar = true;
@@ -144,12 +160,13 @@ function selectProduct(idParm) {
 }
 
 /////////////////////////////////////////////////////////////////////
-// GET TWITTER JSON AND PARSE
+// POPUP ANIMATIONS
 /////////////////////////////////////////////////////////////////////
 
 var incrementFactorArray = new Array();
 var incrementFactorSumVar = 2;
 incrementFactorArray[0] = 2
+// Calculate curved path for popup animation (done once on initial load).
 for (i = 1; i < transitionPositionStepsVar; i++) {
     incrementFactorArray[i] = Math.pow(incrementFactorArray[(i - 1)], transitionCurveVar);
     incrementFactorSumVar = incrementFactorSumVar + incrementFactorArray[i];
@@ -185,6 +202,7 @@ function startTransitionPosition() {
     transitionPosition();
 }
 
+// Loop through transition position by setting timeouts.
 function transitionPosition() {
     popupObjVar.style.top = currentTopVar + "px";
     popupObjVar.style.left = currentLeftVar + "px";   
@@ -210,6 +228,7 @@ function transitionPosition() {
     }  
 }
 
+// Loop through transition size by setting timeouts.
 function transitionSize() {
     document.getElementById("popupProductC4R1Spacer").style.width = currentWidthVar + "px";
     document.getElementById("popupProductC1R3Spacer").style.height = currentHeightVar + "px";    
@@ -246,6 +265,7 @@ function hidePopup() {
     startTransitionPosition();
 }    
 
+// Get position of original panel to be animated.
 function getOffset(el) {
     var _x = 0;
     var _y = 0;
@@ -263,12 +283,10 @@ function getOffset(el) {
 
 var twitterArray;
 
+// Call REST service Java code extracting appropriate records from the Twitter JSON file based on harshtagParm.  The "/%23" adds a "#" to the beginning of the hashtag search string.
 function getTwitter(harshtagParm) {
     twitterArray = new Array();
-//    $.getJSON('js/twitter.json', function (data) {
-    $.getJSON(tweetServiceBaseURL + harshtagParm, function (data) {
-
-
+    $.getJSON(tweetServiceBaseURL + "/%23" + harshtagParm, function (data) {
         try{
             holder =data;
             buildTwitterArray();
@@ -283,22 +301,34 @@ function getTwitter(harshtagParm) {
     
     function buildTwitterArray() { 
         indexVar = 0;
+        // Loop through all the JSON Twitter records returned by the Twitter REST service
         $.each(holder.tweets, function(index, details) {       
             if (details.text) { 
-//                if (details.text.search("#" + hashtagVar) > -1) {                                
+                var dateSortString = details.timestamp_ms;                               
                 var theDate = new Date(parseInt(details.timestamp_ms)); 
-                var month = monthArray[theDate.getMonth()];        
+                var month = monthArray[theDate.getMonth()];                  
+                var hour = theDate.getHours(); if((hour<7)&&(theDate.getMinutes()<50)){hour=hour+9;dateSortString=(dateSortString*1)+32400000;}                
+                var ampmVar = ampmArray[Math.floor(hour/12)]; 
+                hour = hour%12; if (hour == 0) {hour = 12;}                     
                 var minute = "0" + theDate.getMinutes(); minute = minute.substr(-2);        
-                var dateString = theDate.getHours()%12 + ":" + minute + " " + ampmArray[Math.floor(theDate.getHours()/12)] + " - " + month + " " +
-                    theDate.getDate() + ", " + theDate.getFullYear()                                
+                var dateString = hour + ":" + minute + " " + ampmVar + " - " + month + " " +
+                    theDate.getDate() + ", " + theDate.getFullYear(); 
+                 var iVar = details.id_str;var rpVar=iVar.charAt(2)%3;var rtVar=iVar.charAt(4)%2;var lVar=iVar.charAt(6)%5;
+                    if(rpVar==0){rpVar=" ";}if(rtVar==0){rtVar=" ";}if(lVar==0){lVar=" ";}
+                // Load the twitterArray with data from the Twitter REST service.  Also create a starting value for the tweetHTML string based on a previously loaded tweetTableTemplateVar.                                 
                 twitterArray[indexVar] = {screenName: details.user.screen_name.trim(), name: details.user.name.trim(),  tweetText: formatTweetText(details.text), 
-                     tweetTimestamp: details.timestamp_ms, tweetDate: dateString, id_str: details.id_str, tweetHTML: tweetTableTemplateVar};                     
+                     tweetTimestamp: dateSortString, tweetDate: dateString, id_str: details.id_str, tweetHTML: tweetTableTemplateVar};  
+                // Replace placeholders in the tweetHTML string with specific values for the Tweet record.                        
                 twitterArray[indexVar].tweetHTML =  twitterArray[indexVar].tweetHTML.replace("~NAME~", twitterArray[indexVar].name);  
-                twitterArray[indexVar].tweetHTML =  twitterArray[indexVar].tweetHTML.replace("~SCREENNAME~", twitterArray[indexVar].screenName);                  
+                twitterArray[indexVar].tweetHTML =  twitterArray[indexVar].tweetHTML.replace("~SCREENNAME~", "<a href=\"https://twitter.com/" + 
+                    twitterArray[indexVar].screenName + "\" class=\"tweetScreennameLink\" target=\"_blank\" >@" + twitterArray[indexVar].screenName + "</>");                    
+                twitterArray[indexVar].tweetHTML =  twitterArray[indexVar].tweetHTML.replace("~FOLLOWSCREENNAME~", twitterArray[indexVar].screenName);                   
                 twitterArray[indexVar].tweetHTML =  twitterArray[indexVar].tweetHTML.replace("~TEXT~", twitterArray[indexVar].tweetText);     
-                 twitterArray[indexVar].tweetHTML =  twitterArray[indexVar].tweetHTML.replace("~DATE~", twitterArray[indexVar].tweetDate);                                                          
-                indexVar = indexVar + 1;   
-//                }                                           
+                twitterArray[indexVar].tweetHTML =  twitterArray[indexVar].tweetHTML.replace("~DATE~", twitterArray[indexVar].tweetDate);               
+                twitterArray[indexVar].tweetHTML =  twitterArray[indexVar].tweetHTML.replace("~REPLIES~", "<input type=\"text\" readonly value=\"" + rpVar + "\" />"); 
+                twitterArray[indexVar].tweetHTML =  twitterArray[indexVar].tweetHTML.replace("~RETWEETS~", "<input type=\"text\" readonly value=\"" + rtVar + "\" />"); 
+                twitterArray[indexVar].tweetHTML =  twitterArray[indexVar].tweetHTML.replace("~LIKES~", "<input type=\"text\" readonly value=\"" + lVar + "\" />");                                                     
+                indexVar = indexVar + 1;                                            
             }                                              
         });            
         twitterDataLoadedVar = true; 
@@ -308,41 +338,28 @@ function getTwitter(harshtagParm) {
     }    
 }
 
+// Create the Tweet table HTML based on the tweetHTML strings.  Sort this array and then create the final Tweet table HTML.
 function buildTwitterHTML(sortParm) {
-/*        var tweetSortArray = new Array();
-        tweetSortArray[1] = [0, 1, 2];
-        tweetSortArray[2] = [1, 0, 2];
-        tweetSortArray[3] = [2, 0, 1];
-        var sortVar = tweetSortArray[sortParm];
-        var tweetHeaderArray = new Array();
-        tweetHeaderArray[0] = "<td class=\"tweetHeader\"><img src=\"Images/spacer.png\" class=\"twitterScreenName\" />Screen Name</td>";
-        tweetHeaderArray[1] = "<td class=\"tweetHeader\"><img src=\"Images/spacer.png\" class=\"twitterTweetText\" />Tweet Text</td>";
-        tweetHeaderArray[2] = "<td class=\"tweetHeader\"><img src=\"Images/spacer.png\" class=\"twitterTweetDate\" />Tweet Date</td>";
-        var tweetString = "<table class=\"twitterTable\"><tr>" + tweetHeaderArray[sortVar[0]] + tweetHeaderArray[sortVar[1]] + tweetHeaderArray[sortVar[2]] + "</tr>";  
-*/           
-var tweetString = "";      
-        var tweetDisplayArray = new Array();
-//        var tweetRowArray = new Array();       
-        for (i = 0; i < twitterArray.length; i++) {   
-        /*      
-            tweetRowArray[0] = "<td>" +  twitterArray[i].screenName + "</td>";               
-            tweetRowArray[1] = "<td>" +  formatTweetText(twitterArray[i].tweetText) + "</td>";   
-            tweetRowArray[2] = "<td>" +  twitterArray[i].tweetDate + "</td>";      
-            tweetDisplayArray[i] = "<tr>" + tweetRowArray[sortVar[0]] + tweetRowArray[sortVar[1]] + tweetRowArray[sortVar[2]] + "</tr>"; 
-            */           
-         tweetDisplayArray[i] = twitterArray[i].tweetHTML + "<br />";                 
-        }        
-//        tweetDisplayArray.sort(function (a, b) {
-//            return a.toLowerCase().localeCompare(b.toLowerCase());
-//        });       
-        for (i = 0; i < tweetDisplayArray.length; i++) {  
-            tweetString = tweetString + tweetDisplayArray[i];
+        var sortHiddenStringVar;        
+        var tweetSortArray = new Array();  
+        var tweetString = "";          
+        for (i = 0; i < twitterArray.length; i++) {  
+            if (sortParm == 1) {sortHiddenStringVar = twitterArray[i].screenName.toUpperCase();}
+            if (sortParm == 2) {sortHiddenStringVar = twitterArray[i].tweetText.toUpperCase();}
+            if (sortParm == 3) {sortHiddenStringVar = twitterArray[i].tweetTimestamp;}           
+            tweetSortArray[i] = sortHiddenStringVar + "^*^" +  twitterArray[i].tweetHTML;
         }
-        tweetString = tweetString + "</table>";        
+        tweetSortArray.sort(); 
+        for (i = 0; i < twitterArray.length; i++) {                          
+            tweetString = tweetString + tweetSortArray[i].substr(tweetSortArray[i].indexOf('^*^') + 3);  
+        }
+        tweetString = tweetString + "<br /><br />";        
         document.getElementById("popupTwitterContentDiv").innerHTML = tweetString;        
+        document.getElementById("popupTwitterContentDiv").scrollTo(0, 0);                
         document.getElementById("tweetNumberLabel").innerHTML = indexVar + " Tweets Total";
 }
 
+// Format the raw tweet text.
 function formatTweetText(stringParm) { 
     var tweetVar = "  " + stringParm + "  "; 
     var locationVar;
@@ -354,6 +371,7 @@ function formatTweetText(stringParm) {
         var linkString = "";
         var textString = "";
     }    
+    tweetVar = tweetVar.replace(/’/g, "'");
     tweetVar = tweetVar.replace(/€H1/g, '');    
     tweetVar = tweetVar.replace(/€H2/g, ''); 
     tweetVar = tweetVar.replace(/€H3/g, '');     
@@ -426,8 +444,4 @@ function getBrowser(){
         return M[0];
     }    
 }
-
-
-
-
 
