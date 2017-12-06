bcc3011d5bfe4dcaf83ab9f3c272722db43ada21
wvbirder <wvbirder@comcast.net>
2017-12-06 14:05:11 -0500

    update

--- public/js/alphaOffice.js
+++ public/js/alphaOffice.js
@@ -55,6 +55,7 @@ $(document).ready(function () {
     $.getJSON(dbServiceURL, function (data) {
         try{
             holder =data;
+            console.log(process.env);
             buildHTML();
         }
         catch(err){
