d39dff9c7f54cf2ff8b030e981b13bc76e40d2c6
wvbirder <wvbirder@comcast.net>
2017-12-06 17:39:19 -0500

    updated

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.use(express.static('/pipeline/source/public'));
 app.get('/', function (req, res) {
     // console.log('Request came into container: ' + process.env.OCCS_CONTAINER_NAME);
     res.sendFile(path.join(__dirname + '/public/alpha.html'));
-    if (req.hostname == "140.86.40.237") {
-        console.log("A request came into " + process.env.OCCS_CONTAINER_NAME + " from " + req.hostname);
+    if (req.hostname != "localhost" && req.hostname != process.env.OCCS_HOSTIP) {
+        console.log("A Request from " + process.env.OCCS_CONTAINER_NAME + " from " + req.hostname);
     }
 });
 
