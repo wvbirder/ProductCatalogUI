6815585415b801bd8e79e274f7836af1a5988dd5
wvbirder <32797703+wvbirder@users.noreply.github.com>
2018-09-11 16:12:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,16 +6,12 @@ var path = require('path');
 var PORT = process.env.PORT || 8085;
 var app = express();
 
-app.use(express.static('/pipeline/source/public'));
+app.use(express.static('./public'));
 
 app.get('/', function (req, res) {
-    // console.log('Request came into container: ' + process.env.OCCS_CONTAINER_NAME);
     res.sendFile(path.join(__dirname + '/public/alpha.html'));
-    if (req.hostname != "localhost" && req.hostname != process.env.OCCS_HOSTIP) {
-        console.log("A Request from " + process.env.OCCS_CONTAINER_NAME + " from " + req.hostname);
-    }
 });
 
 app.listen(PORT, function () {
-    console.log('listening on port ' + PORT);
+    console.log('listening on port ' + PORT)
 });
